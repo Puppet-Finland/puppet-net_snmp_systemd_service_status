1f18ef6d8b8ffe2f28de9f2c541ae13fedbccaac
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2021-08-16 18:15:40 +0300

    Fix silliness
    Dependencies can be handled when the class is being included. Plus there was a
    typo in the code that prevented it from working.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,19 +2,12 @@
 # @summary set up net-snmp-systemd-service-status
 #
 class net_snmp_systemd_service_status
-(
-  $my_require = Class['::snmpd::install'],
-  $my_notify = Class['::snmpd::service'],
-) 
 {
-
   file { '/etc/snmp/systemd-service-status.py':
-    ensure     => 'present',
-    content    => template('net_snmp_systemd_service_status/systemd-service-status.py.erb'),
-    owner      => root,
-    group      => root,
-    mode       => 0755,
-    my_require => $require,
-    my_notify  => $notify,
+    ensure  => 'present',
+    content => template('net_snmp_systemd_service_status/systemd-service-status.py.erb'),
+    owner   => root,
+    group   => root,
+    mode    => 0755,
   }
 }
